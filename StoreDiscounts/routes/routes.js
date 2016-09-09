7c093a81d8a49d713e0454611d45d7e9e3cb4a9a
Pawan Araballi <araballi.pawan@gmail.com>
2016-09-08 22:20:14 -0400

    Discounts API

---
+++ StoreDiscounts/routes/routes.js
@@ -0,0 +1,69 @@
+module.exports = require('mysql')
+
+var express = require('express');
+var app = express();
+var bodyParse = require('body-parser');
+var msql = require('mysql');
+
+
+app.use(bodyParse.urlencoded({extended: true}));
+app.use(bodyParse.json());
+
+
+module.exports = function(app) {      
+
+
+     app.get('/', function(req, res) {       
+
+          res.end("Welcome to our API");    
+     });
+
+     app.get('/datafetch',function(req,res){    
+     var msg = ''; 
+          function handleDisconnect() {
+          var connection = msql.createConnection({
+               host : 'mydbase.cwgnanpueibv.us-east-1.rds.amazonaws.com',
+               user : 'indra',
+               password : 'qqqqqqqq',
+               port : '3306',
+               database: "discount_data"
+          });
+          connection.connect(function(err){
+              if(err){
+                  console.log('Error while connecting to database:',err);
+                  setTimeout(handleDisconnect,2000);
+              }else{
+              
+ 
+              //   connection.query(queryString, function(err, rows, fields) {
+              //     if (err) throw err;
+               
+              //     for (var i in rows) {
+              //         console.log('Post Titles: ', rows[i].discount);
+              //     }
+              // });
+
+              
+              connection.query('SELECT * FROM data',function(err,rows){
+                if(err) throw err;
+
+                console.log('Data received from Db:\n');
+                console.log(rows);
+                res.json({discounts: rows});
+
+
+                      });
+              }
+          });
+          connection.on('error', function(err) {
+              console.log('db error', err);
+              if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+                   handleDisconnect();
+              } else {                                      
+                throw err;                                  
+              }
+            });
+          }
+          handleDisconnect();
+     });     
+};
